f5ddd4079e144965a5ec470892cb62a8e155c6c3
DygelnyyKirill <dygelniy.1994kirill@gmail.com>
2020-06-16 21:42:13 +0300

    16.06.2020 work

--- js/catalog.js
+++ js/catalog.js
@@ -1,6 +1,15 @@
 const selectElement = document.querySelector("[data-select]");
 
 selectElement.addEventListener("click", function(event){
-    console.log(event);
-    console.log(event.target);
+    
+    if (event.target.hasAttribute("data-select-item")) {
+        console.log("click on item");
+        const itemTitle = event.target.getAttribute("data-select-item"); 
+        console.log(itemTitle)
+        event.target.closest("[data-select]").querySelector("[data-select-title]").textContent = itemTitle;
+        event.target.closest("[data-select]").querySelector(".header-select-dropdawn").classList.toggle("hidden");
+    } else{
+        console.log("click on title and around");
+        this.querySelector(".header-select-dropdawn").classList.toggle("hidden");
+     }  
 })
